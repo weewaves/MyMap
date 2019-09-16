1dad6d84df0ebbce586d6984899e9724b49741ad
Luke Tran <weewaves@gmail.com>
2019-09-16 21:57:19 +0700

    Create waypoint dialog data

--- MyMap.Client/src/app/component/component.module.ts
+++ MyMap.Client/src/app/component/component.module.ts
@@ -3,15 +3,21 @@ import { CommonModule } from '@angular/common';
 import { AgmCoreModule } from '@agm/core';
 import { environment } from 'src/environments/environment';
 import { MapComponent } from './map/map.component';
+import { MaterialModule } from 'src/app/shared/material.module';
+import { WayPointDialogComponent } from './way-point-dialog/way-point-dialog.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   imports: [
     CommonModule,
+    BrowserAnimationsModule,
+    MaterialModule,
     AgmCoreModule.forRoot({
       apiKey: environment.googleMapAPIKey
     })
   ],
-  declarations: [MapComponent],
-  exports: [MapComponent]
+  entryComponents: [WayPointDialogComponent],
+  declarations: [MapComponent, WayPointDialogComponent],
+  exports: [MapComponent, WayPointDialogComponent]
 })
 export class ComponentModule { }
--- MyMap.Client/src/app/component/map/map.component.ts
+++ MyMap.Client/src/app/component/map/map.component.ts
@@ -4,12 +4,14 @@ import { MouseEvent, AgmMap, AgmMarker } from '@agm/core';
 import { MapService } from 'src/app/service/common/map.service';
 import { WayPointService } from 'src/app/service/way-point/waypoint.service';
 import { WayPointViewModel } from 'src/app/model/view-model/way-point/way-point-view-model';
+import { MatDialog } from '@angular/material';
+import { WayPointDialogComponent } from '../way-point-dialog/way-point-dialog.component';
 
 declare var google: any;
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.css']
+  styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements OnInit {
   public get mapService(): MapService {
@@ -18,69 +20,112 @@ export class MapComponent implements OnInit {
   public set mapService(value: MapService) {
     this._mapService = value;
   }
-  @ViewChild('GoogleMap') googleMap: AgmMap;
-  @ViewChild('MakerLayer') markerLayer: AgmMarker;
+  @ViewChild('GoogleMap', { static: true }) googleMap: AgmMap;
+  @ViewChild('MakerLayer', { static: true }) markerLayer: AgmMarker;
 
 
   private _googleMapElement: any;
-  private _idleEventListener: any;
+  private _mapBoundChangedEventListener: any;
 
   // Default settings
   zoom = 8;
   lat = 51.673858;
   lng = 7.815982;
 
-  markers: WayPointViewModel[] = [];
+  markers: WayPointViewModel[] = null;
 
-  constructor(private _mapService: MapService, private _waypointService: WayPointService) {
+  constructor(
+    private _mapService: MapService,
+    private _waypointService: WayPointService,
+    public dialog: MatDialog) {
   }
 
   ngOnInit(): void {
     this.googleMap.mapReady.subscribe(this._onMapReady.bind(this));
   }
 
-  _onMapReady(event: any) {
-    this._googleMapElement = event;
-    this._idleEventListener = google.maps.event.addListener(this._googleMapElement, 'idle', this._onMapIdle.bind(this));
+  getMarkerCollection(): WayPointViewModel[] {
+    return this.markers;
   }
 
-  _onMapIdle() {
+  clickedMarker(wayPointData: WayPointViewModel, index: number) {
+    const dialogRef = this.dialog.open(WayPointDialogComponent, {
+      width: '250px',
+      data: wayPointData
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(WayPointDialogComponent, {
+      width: '250px',
+      data: null
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
+
+  mapClicked($event: MouseEvent) {
+    const self = this;
+    const clickedData: WayPointViewModel = {
+      name: '',
+      lat: $event.coords.lat,
+      lng: $event.coords.lng,
+      draggable: true
+    };
+
+    const dialogRef = this.dialog.open(WayPointDialogComponent, {
+      width: '250px',
+      data: clickedData
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+
+    // this._waypointService
+    //   .createWayPoint(clickedData)
+    //   .subscribe(() => {
+    //     self.markers.push({
+    //       lat: $event.coords.lat,
+    //       lng: $event.coords.lng,
+    //       draggable: true
+    //     });
+    //   });
+  }
+
+  markerDragEnd(m: WayPointViewModel, $event: MouseEvent) {
+    console.log('dragEnd', m, $event);
+  }
+
+  markerDouleClick(m: WayPointViewModel, $event: MouseEvent) {
+    console.log('double click', m, $event);
+  }
+
+  _onMapReady(event: any) {
+    this.markers = [];
+    this._googleMapElement = event;
+    this._mapBoundChangedEventListener = google
+      .maps
+      .event
+      .addListener(this._googleMapElement, 'bounds_changed', this._onMapBoundChanged.bind(this));
+  }
+
+  _onMapBoundChanged() {
     const self = this;
     const currentMapRegion = this._mapService.getCurrentBound(this._googleMapElement);
 
     this._waypointService
       .loadRegionalWayPointCollection(currentMapRegion)
       .subscribe(res => {
-        self.markers = [];
         setTimeout(() => {
           self.markers = res;
-        });
+        }, 10);
       });
-  }
-
-  clickedMarker(label: string, index: number) {
-    console.log(`clicked the marker: ${label || index}`);
-  }
-
-  mapClicked($event: MouseEvent) {
-    const self = this;
-    const data: WayPointViewModel = {
-      lat: $event.coords.lat,
-      lng: $event.coords.lng,
-      draggable: true
-    };
-    this._waypointService
-      .createWayPoint(data)
-      .subscribe(() => {
-        self.markers.push({
-          lat: $event.coords.lat,
-          lng: $event.coords.lng,
-          draggable: true
-        });
-      });
-  }
-
-  markerDragEnd(m: WayPointViewModel, $event: MouseEvent) {
-    console.log('dragEnd', m, $event);
   }
 }
