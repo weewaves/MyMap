75bee6031259029d738343e90d6b46b6c91a687d
Luke Tran <weewaves@gmail.com>
2019-10-07 15:28:13 +0900

    Create MyMap InitMigration

---
+++ MyMap.DataModel/Migrations/20191007062647_InitMyMapDatabase.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace MyMap.DataModel.Migrations
+{
+    public partial class InitMyMapDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Area",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreatedDate = table.Column<DateTime>(nullable: false),
+                    Disabled = table.Column<bool>(nullable: false),
+                    LastModifiedDate = table.Column<DateTime>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    Type = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Area", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Spot",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    AreaId = table.Column<Guid>(nullable: true),
+                    CreatedDate = table.Column<DateTime>(nullable: false),
+                    Disabled = table.Column<bool>(nullable: false),
+                    Height = table.Column<float>(nullable: true),
+                    LastModifiedDate = table.Column<DateTime>(nullable: true),
+                    Latitude = table.Column<decimal>(nullable: true),
+                    Longitude = table.Column<decimal>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    PictureUrls = table.Column<List<string>>(nullable: true),
+                    Type = table.Column<int>(nullable: false),
+                    Vote = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Spot", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Spot_Area_AreaId",
+                        column: x => x.AreaId,
+                        principalTable: "Area",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Spot_AreaId",
+                table: "Spot",
+                column: "AreaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Spot");
+
+            migrationBuilder.DropTable(
+                name: "Area");
+        }
+    }
+}
